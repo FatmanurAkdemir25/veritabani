977428b27d83c53625b7529b67c009378a3f6477
Fatmanur Akdemir <fatmanurakdemir25@gmail.com>
2024-12-09 22:28:41 +0300

    gereksiz connect baglantisi ve transcript silindi

--- src/main/java/edu/university/coursemanagement/CourseManagementSystemApplication.java
+++ src/main/java/edu/university/coursemanagement/CourseManagementSystemApplication.java
@@ -2,24 +2,17 @@ package edu.university.coursemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.sql.Connection;// JDBC Connection sınıfını import ettik
-import edu.university.coursemanagement.connection.DatabaseConnection;  // DatabaseConnection sınıfını import ettik
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
+@EntityScan(basePackages = "edu.university.coursemanagement.model")
+@EnableJpaRepositories(basePackages = "edu.university.coursemanagement.repository")
 public class CourseManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseManagementSystemApplication.class, args);
 
-        // Veritabanı bağlantısını kurmak için DatabaseConnection sınıfını kullanıyoruz
-        DatabaseConnection dbConnection = new DatabaseConnection();
-        Connection connection = dbConnection.getConnection();  // Connection sınıfı burada kullanılıyor
-
-        // Bağlantı başarılı ise mesaj yazdırılır
-        if (connection != null) {
-            System.out.println("Database connection is successful!");
-        } else {
-            System.out.println("Failed to connect to the database.");
-        }
+        
     }
 }
 
